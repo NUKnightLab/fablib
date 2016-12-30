88dcf0713218a375d6641e53d88a1dc0a4eff930
Joe Germuska <joe@germuska.com>
2016-12-30 14:42:35 -0600

    fix arguments to make_zip

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ from .fos import clean, exists, join
 from .utils import notice, warn, abort, do, confirm
 from . import aws, git, static
 
-   
+
 if not 'project_name' in env:
     abort('You must set env.project_name in your fabfile')
 
@@ -34,13 +34,13 @@ env.s3cmd_cfg = join(env.sites_path, 'secrets', 's3cmd.cfg')
 if not os.path.exists(env.s3cmd_cfg):
     warn("Could not find 's3cmd.cfg' repository at '%(s3cmd_cfg)s'." \
          "  You will not be able to deploy.")
-                  
+
 #
 # Load config.json from project directory?
 #
 _config = None
 
-config_json_path = join(env.sites_path, env.project_name, 'config.json')  
+config_json_path = join(env.sites_path, env.project_name, 'config.json')
 try:
     _config = static.load_config(config_json_path)
     notice('Loaded config @ %s' % config_json_path)
@@ -49,22 +49,22 @@ except IOError:
 
 
 def _setup_env():
-    """Setup the local working environment."""    
+    """Setup the local working environment."""
     env.home_path = os.path.expanduser('~')
     env.env_path = os.getenv('WORKON_HOME')
-    
+
     if not env.env_path:
         warn("You should set the WORKON_HOME environment variable to" \
              " the root directory for your virtual environments.")
         env.env_path = env.sites_path
-    
+
     env.project_path = join(env.sites_path, env.project_name)
     env.ve_path = join(env.env_path, env.project_name)
-    env.activate_path = join(env.ve_path, 'bin', 'activate') 
-    
+    env.activate_path = join(env.ve_path, 'bin', 'activate')
+
 
 def _s3cmd_put(src_path, bucket):
-    """Copy local directory to S3 bucket"""    
+    """Copy local directory to S3 bucket"""
     with lcd(env.sites_path):
         local('fablib/bin/s3cmd --config=%s put' \
                 ' --rexclude ".*/\.[^/]*$"' \
@@ -75,7 +75,7 @@ def _s3cmd_put(src_path, bucket):
 
 
 def _s3cmd_sync(src_path, bucket):
-    """Sync local directory with S3 bucket"""     
+    """Sync local directory with S3 bucket"""
     with lcd(env.sites_path):
         local('fablib/bin/s3cmd --config=%s sync' \
                 ' --rexclude ".*/\.[^/]*$"' \
@@ -84,15 +84,15 @@ def _s3cmd_sync(src_path, bucket):
                 ' %s/ s3://%s/' \
                 % (env.s3cmd_cfg, src_path, bucket))
 
- 
- 
+
+
 ############################################################
 # JS libraries
 ############################################################
 if _config:
 
-    # Set env.cdn_path = path to cdn repository   
-    env.cdn_path = abspath(join(_config['root_path'], 
+    # Set env.cdn_path = path to cdn repository
+    env.cdn_path = abspath(join(_config['root_path'],
         'cdn.knightlab.com', 'app', 'libs', _config['name']))
 
 
@@ -106,24 +106,24 @@ if _config:
 
 
     @task
-    def build():       
-        """Build lib version"""   
+    def build():
+        """Build lib version"""
         _setup_env()
 
         # Get build config
         if not 'build' in _config:
             abort('Could not find "build" in config file')
-      
+
         # Check version
         if not 'version' in _config:
             _config['version'] = datetime.utcnow().strftime('%Y-%m-%d-%H-%M-%S')
             warn('Using development version value "%(version)s"' % _config)
-    
+
         notice('Building version %(version)s...' % _config)
 
         # Clean build directory
         clean(_config['build_path'])
-        
+
         # Build it
         for key, param in _config['build'].iteritems():
             getattr(static, key)(_config, param)
@@ -131,7 +131,7 @@ if _config:
 
     @task
     def stage():
-        """Build/commit/tag/push lib version, copy to local cdn repo"""    
+        """Build/commit/tag/push lib version, copy to local cdn repo"""
         _setup_env()
 
         if not 'stage' in _config:
@@ -139,19 +139,19 @@ if _config:
 
         # Make sure cdn exists
         exists(dirname(env.cdn_path), required=True)
-    
+
         # Ask user for a new version
         _config['version'] = git.prompt_tag('Enter a new version number',
-            unique=True) 
-             
-        # Build version   
+            unique=True)
+
+        # Build version
         build()
-        
+
         # Commit/push/tag
         with lcd(env.project_path):
             with settings(warn_only=True):
                 local('git add build')
-            # support builds where there's no change; sometimes comes up when 
+            # support builds where there's no change; sometimes comes up when
             # reusing a tag because of an unexpected problem
             with settings(warn_only=True):
                 msg = local('git commit -m "Release %(version)s"' % _config,capture=True)
@@ -161,52 +161,52 @@ if _config:
                 elif not msg.startswith('[master'):
                     abort("Unexpected result: %s" % msg)
             local('git push')
-            
+
             git.push_tag(_config['version'])
-            
-        # Copy to local CDN repository        
+
+        # Copy to local CDN repository
         cdn_path = join(env.cdn_path, _config['version'])
         clean(cdn_path)
-    
+
         for r in _config['stage']:
             static.copy(_config, [{
                 "src": r['src'],
                 "dst": cdn_path, "regex": r['regex']}])
-                
+
         # Create zip file in local CDN repository
-        _make_zip(cdn_path, '%(name)s.zip' % _config)
-  
-            
+        _make_zip(join(cdn_path, '%(name)s.zip' % _config))
+
+
     @task
     def stage_dev():
         """
-        Build lib and copy to local cdn repository as 'dev' version       
+        Build lib and copy to local cdn repository as 'dev' version
         No tagging/committing/etc/
-        """    
+        """
         _setup_env()
-        
+
         if not 'stage' in _config:
             abort('Could not find "stage" in config file')
 
         # Make sure cdn exists
         exists(dirname(env.cdn_path), required=True)
-                 
-        # Build version   
+
+        # Build version
         build()
-                    
-        # Copy to local CDN repository        
+
+        # Copy to local CDN repository
         cdn_path = join(env.cdn_path, 'dev')
         clean(cdn_path)
-    
+
         for r in _config['stage']:
             static.copy(_config, [{
                 "src": r['src'],
                 "dst": cdn_path, "regex": r['regex']}])
-                
+
         # Create zip file in local CDN repository
         _make_zip(join(cdn_path, '%(name)s.zip' % _config))
-        
-                
+
+
     @task
     def stage_latest():
         """Copy lib version to latest within local cdn repo"""
@@ -216,85 +216,85 @@ if _config:
             version = _config['version']
         else:
             version = git.prompt_tag('Which version to stage as "latest"?')
-    
+
         notice('stage_latest: %s' % version)
-    
+
         # Make sure version has been staged
         version_cdn_path = join(env.cdn_path, version)
-        if not os.path.exists(version_cdn_path): 
+        if not os.path.exists(version_cdn_path):
             abort("Version '%s' has not been staged" % version)
-      
-        # Stage version as latest           
+
+        # Stage version as latest
         latest_cdn_path = join(env.cdn_path, 'latest')
         clean(latest_cdn_path)
         static.copy(_config, [{
             "src": version_cdn_path, "dst": latest_cdn_path}])
 
-   
+
     @task
     def untag():
         """Delete a tag (in case of error)"""
         version = git.prompt_tag('Which tag to delete?')
         if not version:
-            abort('No available version tag')     
+            abort('No available version tag')
         git.delete_tag(version)
 
-                
+
 ############################################################
 # Static websites deployed to S3
-############################################################ 
+############################################################
 if _config and 'deploy' in _config:
 
-    @task 
+    @task
     def undeploy(env_type):
         """Delete website from S3 bucket.  Specify stg|prd as argument."""
-        _setup_env() 
+        _setup_env()
 
         # Activate local virtual environment (for render_templates+flask?)
-        local('. %s' % env.activate_path)        
+        local('. %s' % env.activate_path)
 
         if not os.path.exists(env.s3cmd_cfg):
             abort("Could not find 's3cmd.cfg' repository at '%(s3cmd_cfg)s'.")
 
         if not env_type in _config['deploy']:
             abort('Could not find "%s" in "deploy" in config file' % env_type)
-        
+
         if not "bucket" in _config['deploy'][env_type]:
             abort('Could not find "bucket" in deploy.%s" in config file' % env_type)
-        
+
         bucket = _config['deploy'][env_type]['bucket']
 
         warn('YOU ARE ABOUT TO DELETE EVERYTHING IN %s' % bucket)
         if not do(prompt("Are you ABSOLUTELY sure you want to do this? (y/n): ").strip()):
-            abort('Aborting.')   
-             
+            abort('Aborting.')
+
         with lcd(env.sites_path):
             local('fablib/bin/s3cmd --config=%s del -r --force s3://%s/' \
                 % (env.s3cmd_cfg, bucket))
 
 
     @task
-    def render(env_type):   
+    def render(env_type):
         """Render templates (deploy except for actual sync with S3)"""
-        _setup_env() 
-        
+        _setup_env()
+
         # Activate local virtual environment (for render_templates+flask?)
-        local('. %s' % env.activate_path)        
+        local('. %s' % env.activate_path)
 
         if not os.path.exists(env.s3cmd_cfg):
             abort("Could not find 's3cmd.cfg' repository at '%(s3cmd_cfg)s'.")
-                
+
         if not env_type in _config['deploy']:
             abort('Could not find "%s" in "deploy" in config file' % env_type)
-        
+
         if not "bucket" in _config['deploy'][env_type]:
             abort('Could not find "bucket" in deploy.%s" in config file' % env_type)
-           
+
         if 'usemin_context' in _config['deploy'][env_type]:
             usemin_context = _config['deploy'][env_type]['usemin_context']
         else:
             usemin_context = None
-        
+
         template_path = join(_config['project_path'], 'website', 'templates')
         deploy_path = join(_config['project_path'], 'build', 'website')
 
@@ -305,12 +305,12 @@ if _config and 'deploy' in _config:
             deploy_context = _config['deploy'][env_type]['deploy_context']
         else:
             deploy_context = {}
-            
+
         # Sometimes we need this path append to import app from website
         # in render_templates, dunno why:
         sys.path.append(_config['project_path'])
-        
-        static.render_templates(template_path, deploy_path, deploy_context)   
+
+        static.render_templates(template_path, deploy_path, deploy_context)
         static.usemin(_config, [deploy_path], usemin_context)
 
         # Copy static files
@@ -324,32 +324,32 @@ if _config and 'deploy' in _config:
             static.copy(_config, _config['deploy'][env_type]['copy'])
 
 
-    @task 
+    @task
     def put(env_type):
         """Put (copy) website to S3 bucket.  Specify stg|prd as argument."""
         render(env_type)
-        
+
         bucket = _config['deploy'][env_type]['bucket']
         notice('copying to %s' % bucket)
-           
+
         # Copy to S3
         deploy_path = join(_config['project_path'], 'build', 'website')
         _s3cmd_put(deploy_path, bucket)
 
-             
+
     @task
     def deploy(env_type):
-        """Deploy website to S3 bucket.  Specify stg|prd as argument."""       
+        """Deploy website to S3 bucket.  Specify stg|prd as argument."""
         render(env_type)
-                         
+
         bucket = _config['deploy'][env_type]['bucket']
         notice('deploying to %s' % bucket)
-   
+
         # Sync to S3
         deploy_path = join(_config['project_path'], 'build', 'website')
         _s3cmd_sync(deploy_path, bucket)
-         
-    
+
+
 @task
 def serve(ssl='n', port='5000'):
     """Run the development server"""
@@ -358,33 +358,32 @@ def serve(ssl='n', port='5000'):
 
     opts = ' -p '+port
     if do(ssl):
-        opts += ' -s'    
-        
+        opts += ' -s'
+
     with lcd(join(env.project_path)):
         if exists(join(env.project_path, 'manage.py')):
             local('python manage.py runserver')
         elif _config and 'deploy' in _config:
             if int(port) < 1024:
-                local('sudo python website/app.py'+opts)     
+                local('sudo python website/app.py'+opts)
             else:
                 local('python website/app.py'+opts)
-        else:   
+        else:
             if int(port) < 1024:
                 local('sudo python api.py'+opts)
             else:
-                local('python api.py'+opts)                
- 
-                                        
+                local('python api.py'+opts)
+
+
 @task
 def dump():
     """Dump env and config (if applicable) to stdout"""
     import pprint
     import json
-    
+
     notice('dumping env...')
     pprint.pprint(env)
-    
+
     if _config:
         notice('dumping config...')
         print _config
-   
